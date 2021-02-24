f4781f54d75b88096bf1f65860991652c4d1eb76
idembele70 <idembele70@gmail.com>
2021-02-24 20:19:49 +0100

    creation des slides

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,14 @@ import { environment } from '../env.js';
 export class App {
     constructor() {
         this.pageContent = "";
+        this.pizzas =  [];
+        this.slidesTag = null;
+        this.detailsTag = null;
+        this.cartTag = null;
+    }
+
+    get slideWidth(){
+        return (window.innerWidth >= 580) ? window.innerWidth : 580;
     }
 
     async goTo(view) {
@@ -23,20 +31,44 @@ export class App {
 
         this.init(view);
     }
-    init(view){
+    init(view) {
+        this.slidesTag = document.querySelector('.slides');
+        this.detailsTag = document.querySelector('.details');
+        this.cartTag = document.querySelector('.cart');
+
         if (view === 'order-pizza') {
             this.getPizzas();
         }
     }
 
-    getPizzas(){
+    getPizzas() {
         fetch(environment.urlApi)
             .then(res => res.json())
             .then(data => {
                 if (data && data.pizzas) {
-                    console.log(data.pizzas);
+                    this.pizzas = data.pizzas ;
+
+                    for (let pizza of data.pizzas) {
+                        this.createPizzaTag(pizza);
+                        // console.log(pizza);
+                    }
                 }
             });
     }
-}
+    createPizzaTag(pizza) {
+        const pizzaTag = document.createElement('div');
 
+        pizzaTag.className = 'pizza';
+        pizzaTag.style.width = `${this.slideWidth}px`;
+        pizzaTag.innerHTML = `
+        <h1>${pizza.name.toUpperCase()}</h1>
+        <div class="controls">
+            <div class="prev"></div>
+            <div class="next"></div>
+        </div>
+        `;
+
+        this.slidesTag.style.width = `${this.slideWidth*this.pizzas.length}px`;
+        this.slidesTag.appendChild(pizzaTag);
+    }
+}
